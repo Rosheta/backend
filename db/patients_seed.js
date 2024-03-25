eb1e3ec41dab44930beba92edfb4ccadb05a9a0f
m13salama <mohamedsalama236@gmail.com>
2024-03-26 00:15:43 +0200

    finished seeding

--- db/patients_seed.js
+++ db/patients_seed.js
@@ -1,8 +1,7 @@
 const Patient = require('../models/patient');
-const mongoose = require('mongoose');
 const { faker } = require('@faker-js/faker');
 const dotenv = require('dotenv');
-const { writeFile } = require('fs')
+const { writeFile, truncate } = require('fs')
 const db = require('./mongo')
 
 dotenv.config();
@@ -11,39 +10,49 @@ async function main() {
     await Patient.deleteMany({});
     console.log('Database patients Cleared');
 
-    const num_records = 10;
-
+    const num_records = process.env.PATIENTS_SEED_COUNT;
+    truncate("./db/patients.txt", 0, (err) => {
+        if (err) {
+            console.error("Error truncating file:", err);
+        }
+    });
     for (let i = 0; i < num_records; i++) {
         const phone_number = faker.number.int({ min: 10000000000, max: 99999999999 }).toString();
         const ssn = faker.number.int({ min: 10000000000000, max: 99999999999999 }).toString();
         const email = faker.internet.email();
         const password = faker.internet.password();
-        writeFile('./db/patients.txt', `${email} ---> ${password}\n`, {flag: 'a'}, (err) => {
-            if (err)
-                console.log(err);
+        try {
+            const patient = new Patient({
+                name: faker.person.fullName(),
+                phone_number: {
+                    value: phone_number,
+                    visible: true
+                },
+                ssn: {
+                    value: ssn,
+                    visible: true
+                },
+                email: {
+                    value: email,
+                    visible: true
+                },
+                password: password,
+                d_o_b: {
+                    value: faker.date.past(),
+                    visible: true
+                },
+                gender: faker.helpers.arrayElement(['m', 'f']),
+                profile_picture: faker.image.avatar()
             });
-        await Patient.create({
-            name: faker.person.fullName(),
-            phone_number: {
-                value: phone_number,
-                visible: true
-            },
-            ssn: {
-                value: ssn,
-                visible: true
-            },
-            email: {
-                value: email,
-                visible: true
-            },
-            password: password,
-            d_o_b: {
-                value: faker.date.past(),
-                visible: true
-            },
-            gender: faker.helpers.arrayElement(['m','f']),
-            profile_picture: faker.image.avatar()
-        });
+            writeFile('./db/patients.txt', `${patient._id} ---> ${email} ---> ${password}\n`, {flag: 'a'}, (err) => {
+                if (err)
+                    console.log(err);
+            });
+            await patient.save();
+        }
+        catch (error) {
+            console.log(error)
+        }
     }
     console.log("patients seeded successfully")
     db.close()
