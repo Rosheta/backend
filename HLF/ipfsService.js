8bdf2195bbe85b730825eab5ffb16c8617821217
abdu <abdullatifkhalid99@gmail.com>
2024-04-13 18:26:01 +0200

    establish hlf api and ipfs

---
+++ HLF/ipfsService.js
@@ -0,0 +1,85 @@
+const axios = require('axios');
+const dotenv = require('dotenv');
+const fs = require('fs');
+
+dotenv.config();
+ipfsKey = process.env.IPFS_KEY || 'FzfsXnghRuapU5Rg4PkF95oXzvVd1EPT9Al4L8pk';
+ipfsUrl = process.env.IPFS_URL || 'https://65f4cc0c14d31cebc0d4b9c0.ipfs-us-west-2.spydra.app';
+
+
+// upload a file to IPFS
+const headers = {
+    'accept': '*/*',
+    'X-API-KEY': ipfsKey,
+    'Content-Type': '*/*'
+};
+const FormData = require('form-data');
+
+async function uploadFileToIPFS(file) {
+    try {
+        console.log('file', file);
+        const fileSize = fs.statSync(file.path).size;
+        let response;
+        
+        if (fileSize <= 4 * 1024 * 1024) {
+            const formData = new FormData();
+            formData.append('fileName', fs.createReadStream(file.path));
+            
+            response = await axios.post(`${ipfsUrl}/ipfs/add`, formData, {
+                headers: {
+                    'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+                    'accept': '*/*',
+                    'X-API-KEY': ipfsKey
+                }
+            });
+        } else {
+            // Use /ipfs/addByUrl endpoint for files larger than 4MB
+            response = await axios.post(`${ipfsUrl}/ipfs/addByUrl?url=${file.path}`, {}, {
+                headers: {
+                    'accept': '*/*',
+                    'X-API-KEY': ipfsKey
+                }
+            });
+        }
+
+        console.log('File uploaded to IPFS:', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error:', error);
+        return error;
+    }
+}
+
+
+
+// get a file from IPFS
+async function getFileFromIPFS(hash) {
+    
+    try {
+        const response = await axios.post(`${ipfsUrl}/ipfs/cat?arg=${hash}`, null,{ headers });
+        return response.data;
+    } catch (error) {
+        console.error('Error:', error);
+        return error;
+    }
+}
+// delete a file from IPFS
+async function deleteFileFromIPFS(hash) {
+    try {
+        const response = await axios.post(`${ipfsUrl}/ipfs/pin/rm?arg=${hash}`,null,{ headers});
+        return response.data;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+// get all files from IPFS
+ async function getAllFilesFromIPFS() {
+        try {
+            const response = await axios.post(`${ipfsUrl}/ipfs/pin/ls`, null, {headers});
+            console.log('All files from IPFS:', response.data);
+            return response.data;
+        } catch (error) {
+            console.error('Error getting all files from IPFS:', error);
+        }
+    }
+module.exports = { uploadFileToIPFS, getFileFromIPFS, deleteFileFromIPFS, getAllFilesFromIPFS };
--- index.js
+++ index.js
@@ -5,49 +5,90 @@ const userRouter = require('./routers/userRouters.js');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const Patient = require('./models/patient');
-const authController = require('./controller/auth');
-const profileController = require('./controller/profile');
-const chatsController = require('./controller/chats');
-const db = require('./db/mongo')
+// const Patient = require('./models/patient');
+// const authController = require('./controller/auth');
+// const profileController = require('./controller/profile');
+// const chatsController = require('./controller/chats');
+// const db = require('./db/mongo')
 const dotenv = require('dotenv');
 const http = require('http');
-const socketIo = require('socket.io');
+// const socketIo = require('socket.io');
+const hlf = require('./HLF/contractServices');
+const ipfsService = require('./HLF/ipfsService');
 
 dotenv.config();
 const app = express();
 const server = http.createServer(app);
 
-const io = socketIo(server)
-const PORT = process.env.PORT;
+// const io = socketIo(server)
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('lol Rosheta!');
 });
-
-app.use('/', userRouter);
-
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  socket.on('sendMessage', async (data) => {
-      console.log('Message received:', data);
-      senderId = data.senderId;
-      chatId = data.chatId;
-      message = data.message;
-      receiverId =await chatsController.getReceiverId(chatId , senderId);
-      await chatsController.saveMessage(senderId, receiverId, chatId, message);
-    
-      io.emit(`${chatId}`, {
-          sender: senderId,
-          message: message,
-          time: new Date(), 
-          isSeen: false
-      });
-  });
+app.get('/hlf', async (req, res) => {
+  console.log('req.body', req.body);
+  const data = await hlf.getAllMedicalRecords();
+  res.send(data);
 });
+// create a new record
+app.get('/hlf/rec', async (req, res) => {
+  const { id, patientName, dob, diagnosis, medications, allergies, doctor } = req.body;
+  const data = await hlf.CreateMedicalRecord(id, patientName, dob, diagnosis, medications, allergies, doctor);
+  res.send(data);
+});
+
+
+
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' }); // Specify the destination directory for file uploads
+
+app.post('/ipfs/upload', upload.single('file'), async (req, res) => {
+  const file = req.file; // Now, req.file contains the uploaded file information
+  console.log('file', file);
+  const data = await ipfsService.uploadFileToIPFS(file);
+  res.send(data);
+});
+// get a file from IPFS
+app.get('/ipfs/get', async (req, res) => {
+  console.log('req.body', req);
+  const hash = req.query.hash;
+  const data = await ipfsService.getFileFromIPFS(hash);
+  res.send(data);
+});
+app.get('/ipfs/all', async (req, res) => {
+  const data = await ipfsService.getAllFilesFromIPFS();
+  res.send(data);
+ 
+});
+app.get('/ipfs/delete', async (req, res) => {
+  const hash = req.query.hash;
+  const data = await ipfsService.deleteFileFromIPFS(hash);
+  res.send(data);
+} );
+// app.use('/', userRouter);
+
+// io.on('connection', (socket) => {
+//   console.log('A user connected');
+
+//   socket.on('sendMessage', async (data) => {
+//       console.log('Message received:', data);
+//       senderId = data.senderId;
+//       chatId = data.chatId;
+//       message = data.message;
+//       receiverId =await chatsController.getReceiverId(chatId , senderId);
+//       await chatsController.saveMessage(senderId, receiverId, chatId, message);
+    
+//       io.emit(`${chatId}`, {
+//           sender: senderId,
+//           message: message,
+//           time: new Date(), 
+//           isSeen: false
+//       });
+//   });
+// });
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
