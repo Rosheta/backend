64fa133d36e85af223f06e38025aceb3b3e8e3dd
mohamedaiad <mohamedaiad2000@gmail.com>
2024-04-20 13:31:03 +0200

    add search controller

--- controller/search.js
+++ controller/search.js
@@ -5,23 +5,33 @@ dotenv.config();
 
 const searchController = {
     search: async (req, res) => {
-
         const query = req.body.query;
-        const hospital = req.body.hospitalName;
+        const location = req.body.location;
         const specialization = req.body.specialization;
         console.log(query);
-        console.log(hospital);
+        console.log(location);
         console.log(specialization);
-        // const result = await Doctor.find({ username: { $regex: '^' + query, $options: 'i' } },{projection: {username:1}}).toArray();
-        // const result = await Doctor.find({ ssn:{ $regex : '^' + query, $options: 'i'} }).limit(20);
-        const result = await Doctor.find({
-            $or: [
-              { 'name.f_name': { $regex: '^' + query, $options: 'i' } }, // Match first name starting with query (case-insensitive)
-              { 'name.l_name': { $regex: '^' + query, $options: 'i' } }  // Match last name starting with query (case-insensitive)
-            ]
-          }).limit(20);          
+        const filter = {};
+
+        // Construct the query condition based on the provided parameters
+        if (query) {
+            // Search by name approximately matching the query (case-insensitive)
+            filter.name = { $regex: query, $options: 'i' };
+        }
+
+        if (location && location !== 'Any') {
+            // Search by location if specified and not 'Any'
+            filter.location = { $regex: '^' + location, $options: 'i' };
+        }
+
+        if (specialization && specialization !== 'Any') {
+            // Search by specialization if specified and not 'Any'
+            filter.department = { $regex: specialization, $options: 'i' };
+        }
+
+        // Perform the MongoDB query with the constructed filter
+        const result = await Doctor.find(filter).limit(20);
         console.log(result);
-        
 
         if(query == ""){
             return res.status(200).json([]);
