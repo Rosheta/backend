bcafebf3f43c56cc28a773a1b02c993ee90b9f87
mohamedaiad <mohamedaiad2000@gmail.com>
2024-05-14 14:50:26 +0300

    emergency is done but need refactor

--- controller/emergency.js
+++ controller/emergency.js
@@ -1,10 +1,35 @@
 const Patient = require('../models/patient');
 const Government = require('../models/government');
+const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const chatsController = require('../controller/chats');
 const pushNotificationsController = require('./push_notifications');
 
 dotenv.config();
+
+const JWT_REMOTE_ACCESS_SECRET = process.env.JWT_REMOTE_ACCESS_SECRET;
+const JWT_REMOTE_ACCESS_EXPIRE = process.env.JWT_REMOTE_ACCESS_EXPIRE;
+
+// Function to generate a UUID
+function uuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+function generateToken(patientUsername,doctorUsername){
+    const payload = {
+        patientUsername: patientUsername,
+        doctorUsername : doctorUsername,
+        uniqueId: uuid(), // Add a unique identifier to the payload
+    };
+
+    // Generate JWT token
+    const token = jwt.sign(payload, JWT_REMOTE_ACCESS_SECRET, { expiresIn:  JWT_REMOTE_ACCESS_EXPIRE});
+    return token;
+}
 
 const emergencyController = {
     generateToken: async (req, res) => {
@@ -29,7 +54,7 @@ const emergencyController = {
         if(!emergencyMessage) res.status(404).json({ error: 'Error sending emergency message' });
 
         // and generate token to this user to see the data
-        const token = pushNotificationsController.generateToken(); 
+        const token = generateToken(patient.username,government.username); 
         res.status(200).json({
             token: token,
         });
